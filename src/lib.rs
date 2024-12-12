fb4a8af85d2606832a252ed531cb302093e7af27
Lola Aimar <lolaaimar14@gmail.com>
2024-12-12 17:36:15 -0300

    feat #12: added outliers filtering, updated aggregationStatus

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,15 @@ pub use pallet::*;
 
 use frame_support::pallet_prelude::{BoundedVec, ConstU32};
 use frame_system::pallet_prelude::BlockNumberFor;
-use scale_info::prelude::vec::Vec;
+use scale_info::prelude::{vec, vec::Vec};
 use sp_core::crypto::KeyTypeId;
 
 pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"orac");
 
 mod price_providers;
 use price_providers::{PriceProvider, CryptoCompareProvider};
+
+pub const SCALING_FACTOR: f64 = 10000.0;
 
 pub mod crypto {
     use super::KEY_TYPE;
@@ -57,6 +59,7 @@ pub mod pallet {
     use scale_info::prelude::vec;
     use sp_runtime::offchain::{http};
     use sp_runtime::sp_std::str;
+    use scale_info::{prelude::fmt, TypeInfo};
 
     #[pallet::pallet]
     pub struct Pallet<T>(_);
@@ -79,6 +82,9 @@ pub mod pallet {
     #[pallet::storage]
     pub type OutliersRange<T> = StorageValue<_, u32>;
 
+    #[pallet::storage]
+    pub type DivergencePercentage<T> = StorageValue<_, u32>;
+
     /// NodesPrices store latest price for each node
     /// about Identity hasher https://docs.substrate.io/build/runtime-storage/#common-substrate-hashers
     #[pallet::storage]
@@ -93,7 +99,7 @@ pub mod pallet {
     /// The first value is the median price
     /// The second value is the age of the median price
     #[pallet::storage]
-    pub type Price<T> = StorageValue<_, (u32, BlockNumberFor<T>)>;
+    pub type Price<T> = StorageValue<_, (u32, u16)>;
 
     /// oracle genesis config definition and associated macros
     // see https://docs.substrate.io/reference/how-to-guides/basics/configure-genesis-state/
@@ -102,6 +108,7 @@ pub mod pallet {
         pub min_nodes_for_trusted_aggregation: u32,
         pub feed_age: u16,
         pub outliers_range: u32,
+        pub divergence_percentage: u32,
         // Ties `T` to `GenesisConfig` because is needed for `impl<T: Config> BuildGenesisConfig ...`
         _marker: PhantomData<T>,
     }
@@ -112,6 +119,7 @@ pub mod pallet {
                 min_nodes_for_trusted_aggregation: Default::default(),
                 feed_age: Default::default(),
                 outliers_range: Default::default(),
+                divergence_percentage: Default::default(),
                 _marker: Default::default(),
             }
         }
@@ -123,25 +131,28 @@ pub mod pallet {
             <MinNodesForTrustedAggregation<T>>::put(&self.min_nodes_for_trusted_aggregation);
             <FeedAge<T>>::put(&self.feed_age);
             <OutliersRange<T>>::put(&self.outliers_range);
+            <DivergencePercentage<T>>::put(&self.divergence_percentage);
         }
     }
 
-    // Error messages
-    #[derive(Clone, PartialEq, Encode, Decode, MaxEncodedLen, TypeInfo, Debug)]
-    pub enum ErrorMessage {
-        NotEnoughNodes,
-        NoPreviousMedian,
+    // Information about whether the aggregation happened or not
+    #[derive(Clone, PartialEq, Encode, Decode, TypeInfo, Debug)]
+    pub enum Status {
+        AggregationPerformed {
+            non_outliers: u16,
+            non_outlier_prices: Vec<u32>,
+            outliers: u16,
+            outlier_prices: Vec<u32>,
+        },
+        AggregationNotPerformed,
     }
 
     // Aggregation status flag
-    // Used to include more information about an uncommon aggregation if it occurs.
     #[derive(Clone, PartialEq, Encode, Decode, MaxEncodedLen, TypeInfo, Debug)]
-    pub enum Flag<T: Config> {
+    pub enum Flag {
         Ok,
-        Error {
-            message: ErrorMessage,
-            price_age: BlockNumberFor<T>,
-        },
+        NotEnoughNodes,
+        NoPreviousMedian,
     }
 
     /// pallet events
@@ -154,16 +165,13 @@ pub mod pallet {
             when: BlockNumberFor<T>,
         },
         AggregationStatus {
-            median: u32,
+            flag: Flag,
+            participating_nodes: u32,
+            median_price: u32,
+            age: u16,
             block: BlockNumberFor<T>,
-            flag: Flag<T>,
-            non_outliers: Vec<u32>,
+            status: Status,
         },
-    }
-
-    #[pallet::error]
-    pub enum Error<T> {
-        InvalidOracleConfig,
     }
 
     /// pallet calls
@@ -239,44 +247,50 @@ pub mod pallet {
 
         fn on_finalize(n: BlockNumberFor<T>) {
             log::info!("Aggregating median price for block {:?}", n);
-            if let Some((min_nodes_for_trusted_aggregation, feed_age, _outliers_range)) =
-                Self::get_oracle_config()
+            if let Some((
+                min_nodes_for_trusted_aggregation,
+                feed_age,
+                outliers_range,
+                divergence_percentage,
+            )) = Self::get_oracle_config()
             {
-                let mut count: u32 = 0;
+                let mut participating_nodes: u32 = 0;
                 let prices = NodesPrices::<T>::iter_values()
                     .by_ref()
                     .filter_map(|(p, a)| {
                         if (n - a) <= feed_age.into() {
-                            count += 1;
+                            participating_nodes += 1;
                             Some(p)
                         } else {
                             None
                         }
                     })
                     .collect();
-                let (median, age, flag): (u32, BlockNumberFor<T>, Flag<T>) =
-                    if min_nodes_for_trusted_aggregation <= count {
+                let (median_price, age, flag, status): (u32, u16, Flag, crate::Status) =
+                    if min_nodes_for_trusted_aggregation <= participating_nodes {
                         log::info!(
-                            "{:?} nodes have submitted prices. Calculating median..",
-                            count
+                            "{:?} nodes submitted a price. Aggregating median price ...",
+                            participating_nodes
                         );
-                        Self::calculate_median(prices)
+                        Self::aggregate(prices, outliers_range, divergence_percentage)
                     } else {
                         log::error!("Not enough nodes for trusted aggregation. Reusing median ...");
                         Self::reuse_previous_median()
                     };
-                Price::<T>::put((median, age));
+                Price::<T>::put((median_price, age));
                 log::info!(
                     "Median price for block {:?} is {:?} with status: {:?}",
                     n,
-                    median,
+                    median_price,
                     flag
                 );
                 Self::deposit_event(Event::AggregationStatus {
-                    median,
-                    block: n,
                     flag,
-                    non_outliers: vec![],
+                    participating_nodes,
+                    median_price,
+                    age,
+                    block: n,
+                    status,
                 })
             } else {
                 log::error!("Couldn't fetch Oracle Config");
@@ -286,53 +300,73 @@ pub mod pallet {
 }
 
 impl<T: Config> Pallet<T> {
-    fn calculate_median(prices: Vec<u32>) -> (u32, BlockNumberFor<T>, Flag<T>) {
-        let mut sorted_prices = BoundedVec::<u32, ConstU32<32>>::truncate_from(prices);
-        sorted_prices.sort();
-
-        let length = sorted_prices.len();
-        let zero: BlockNumberFor<T> = (0 as u32).into();
-        let median: u32;
-
-        if length % 2 == 0 {
-            median = sorted_prices[(length - 1) / 2];
-        } else {
-            median = (sorted_prices[(length - 1) / 2] + sorted_prices[length / 2]) / 2;
-        }
-
-        (median, zero, Flag::<T>::Ok)
+    fn aggregate(
+        prices: Vec<u32>,
+        outliers_range: u32,
+        divergence_percentage: u32,
+    ) -> (u32, u16, Flag, crate::Status) {
+        let mut prices = BoundedVec::<u32, ConstU32<32>>::truncate_from(prices);
+        prices.sort();
+        let sorted_prices = prices.to_vec();
+        let length: usize = prices.len();
+        let median = Self::calculate_median(sorted_prices.clone(), length);
+        let (non_outlier_prices, outlier_prices) = Self::filter_outliers(
+            sorted_prices,
+            median,
+            length,
+            outliers_range,
+            divergence_percentage,
+        );
+        (
+            median,
+            0,
+            Flag::Ok,
+            Status::AggregationPerformed {
+                non_outliers: non_outlier_prices.len() as u16,
+                non_outlier_prices,
+                outliers: outlier_prices.len() as u16,
+                outlier_prices,
+            },
+        )
     }
 
-    fn reuse_previous_median() -> (u32, BlockNumberFor<T>, Flag<T>) {
+    fn reuse_previous_median() -> (u32, u16, Flag, crate::Status) {
         if let Some((median, age)) = Price::<T>::get() {
-            let one: u32 = 1;
             (
                 median,
-                age + one.into(),
-                Flag::<T>::Error {
-                    message: ErrorMessage::NotEnoughNodes,
-                    price_age: age + one.into(),
-                },
+                age + 1,
+                Flag::NotEnoughNodes,
+                Status::AggregationNotPerformed,
             )
         } else {
             log::error!("Error: no median to reuse.");
-            let zero: BlockNumberFor<T> = (0 as u32).into();
             (
                 0,
-                zero,
-                Flag::<T>::Error {
-                    message: ErrorMessage::NoPreviousMedian,
-                    price_age: zero,
-                },
+                0,
+                Flag::NoPreviousMedian,
+                Status::AggregationNotPerformed,
             )
         }
     }
 
-    fn get_oracle_config() -> Option<(u32, u16, u32)> {
+    fn calculate_median(prices: Vec<u32>, length: usize) -> u32 {
+        if length % 2 == 0 {
+            prices[(length - 1) / 2]
+        } else {
+            (prices[(length - 1) / 2] + prices[length / 2]) / 2
+        }
+    }
+
+    fn get_oracle_config() -> Option<(u32, u16, u32, u32)> {
         if let Some(min_nodes) = MinNodesForTrustedAggregation::<T>::get() {
             if let Some(feed_age) = FeedAge::<T>::get() {
                 if let Some(outliers_range) = OutliersRange::<T>::get() {
-                    Some((min_nodes, feed_age, outliers_range))
+                    if let Some(divergence_percentage) = DivergencePercentage::<T>::get() {
+                        Some((min_nodes, feed_age, outliers_range, divergence_percentage))
+                    } else {
+                        log::error!("Error fetching DivergencePercentage");
+                        None
+                    }
                 } else {
                     log::error!("Error fetching OutliersRange");
                     None
@@ -344,6 +378,48 @@ impl<T: Config> Pallet<T> {
         } else {
             log::error!("Error fetching MinNodesForTrustedAggregation");
             None
+        }
+    }
+
+    fn filter_outliers(
+        prices: Vec<u32>,
+        median: u32,
+        length: usize,
+        outliers_range: u32,
+        divergence: u32,
+    ) -> (Vec<u32>, Vec<u32>) {
+        let first_quartile = Self::calculate_median(
+            prices.clone().into_iter().take(length / 2).collect(),
+            length / 2,
+        );
+        let third_quartile = Self::calculate_median(
+            prices.clone().into_iter().skip(length / 2).collect(),
+            length / 2,
+        );
+
+        let interquartile_range = third_quartile - first_quartile;
+
+        let lower_bound = first_quartile - (outliers_range * interquartile_range);
+        let upper_bound = third_quartile + (outliers_range * interquartile_range);
+
+        prices.into_iter().partition(|x| {
+            (lower_bound <= *x && *x <= upper_bound)
+                && Self::within_divergence(*x, median, divergence)
+        })
+    }
+
+    fn within_divergence(x: u32, median: u32, divergence: u32) -> bool {
+        let dif = Self::unsigned_sub(x, median);
+        let fraction = (f64::from(dif) * SCALING_FACTOR) / f64::from(median);
+        fraction <= divergence.into()
+    }
+
+    // substraction between two u32 can cause overflow
+    fn unsigned_sub(x: u32, y: u32) -> u32 {
+        if x <= y {
+            y - x
+        } else {
+            x - y
         }
     }
 }
